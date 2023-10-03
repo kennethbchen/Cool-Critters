5a76b4fe16e7636794919ecb96aa16859b899f64
kennethbchen <kennethbchen@gmail.com>
2023-10-02 20:49:04 -0400

    Fixed spelling error in logo

--- scripts/TitleLogo.gd
+++ scripts/TitleLogo.gd
@@ -7,7 +7,7 @@ func _ready():
 	var scale_tween = get_tree().create_tween()
 	scale_tween.set_trans(Tween.TRANS_QUAD)
 	scale_tween.set_ease(Tween.EASE_IN_OUT)
-	scale_tween.tween_property(self, "scale", Vector2(.35, .45), 0.6)
+	scale_tween.tween_property(self, "scale", Vector2(.3, .4), 0.6)
 	scale_tween.tween_property(self, "scale", Vector2(.65, .55), 0.6)
 	scale_tween.set_loops()
 	
