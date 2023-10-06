0dbfc674bc48f66cb2f7745aa5bd9c46b40c27e6
kennethbchen <kennethbchen@gmail.com>
2023-10-05 23:53:05 -0400

    Added sound effects and music; Made critters only spawn when the rock is moved for the first time

--- Game.gd
+++ Game.gd
@@ -15,24 +15,34 @@ extends Node2D
 
 @onready var critter_parent: Node = $Critters
 
+@onready var sfx_controller: SoundEffectController = $SoundEffectController
+
+var rock_not_moved: bool = true
+
 var rand: RandomNumberGenerator
 
-# Called when the node enters the scene tree for the first time.
+var spawn_point: Vector2
+
 func _ready():
 	rand = RandomNumberGenerator.new()
 	
-	var spawn_point = rock_origin.position + Vector2(rand.randi_range(-rock_spawn_radius, rock_spawn_radius), rand.randi_range(-rock_spawn_radius, rock_spawn_radius))
+	spawn_point = rock_origin.position + Vector2(rand.randi_range(-rock_spawn_radius, rock_spawn_radius), rand.randi_range(-rock_spawn_radius, rock_spawn_radius))
 	
 	var rock = rock_scene.instantiate()
 	add_child(rock)
+	rock.rock_moved.connect(_on_rock_moved)
 	rock.position = spawn_point
 	
+	
+func _spawn_critters():
 	for i in range(0, rand.randi_range(4, 10)):
 		var critter = critter_scene.instantiate()
 		critter_parent.add_child(critter)
 		critter.position = spawn_point + Vector2(rand.randi_range(-critter_spawn_radius, critter_spawn_radius), rand.randi_range(-critter_spawn_radius, critter_spawn_radius))
 
+func _on_rock_moved():
+	if rock_not_moved:
+		rock_not_moved = false
+		_spawn_critters()
+		sfx_controller.play("applause")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- scripts/Rock.gd
+++ scripts/Rock.gd
@@ -4,12 +4,15 @@ var grab_pos: Vector2
 
 var held = false
 
+signal rock_moved()
+
 # This click and drag setup does not work with overlapping objects
 
 func _unhandled_input(event):
 	
 	if held and event is InputEventMouseMotion:
 		position = event.position - grab_pos
+		rock_moved.emit()
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
 	
